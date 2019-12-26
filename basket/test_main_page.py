66fdfed2e159c0a0f14e94510716a206686e0b1e
Roman Geraskin <roman.n.geraskin@example.com>
2019-12-26 16:59:54 +0300

    PageObject style test rewrite

--- basket/test_main_page.py
+++ basket/test_main_page.py
@@ -1,8 +1,8 @@
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+from basket.pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
