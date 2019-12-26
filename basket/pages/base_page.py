ae4a9848cf280665b48ba7e312dd597b0158de5f
Roman Geraskin <roman.n.geraskin@example.com>
2019-12-26 18:06:32 +0300

    login page described

--- basket/pages/base_page.py
+++ basket/pages/base_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BasePage():
-    def __init__(self, browser: WebDriver, url: str, timeout: int = 10):
+    def __init__(self, browser: WebDriver, url: str, timeout: int = 3):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
