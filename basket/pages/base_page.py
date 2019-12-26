7de941a91ab70d3ef2b856eef768f997a1469c69
Roman Geraskin <roman.n.geraskin@example.com>
2019-12-26 16:47:59 +0300

    base page

---
+++ basket/pages/base_page.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class BasePage():
+    def __init__(self, browser: WebDriver, url: str):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
